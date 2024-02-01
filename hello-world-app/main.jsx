a5f8f52619656f7c9cfd0335e7414e09eb5375ee
Sebastian Romero <nakosume@gmail.com>
2024-01-31 19:31:15 -0500

    little progress

--- hello-world-app/main.jsx
+++ hello-world-app/main.jsx
@@ -2,6 +2,8 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import React from 'react'
+import {createRoot} from'react-dom/client'
 
 document.querySelector('#app').innerHTML = `
   <div>
