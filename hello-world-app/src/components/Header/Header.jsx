df23a7d413f8f5118620c3a819c85856e70006a5
Sebastian Romero <nakosume@gmail.com>
2024-02-12 20:54:28 -0500

    Dropdown menues can be toggled by clicking Header logo/icon

--- hello-world-app/src/components/Header/Header.jsx
+++ hello-world-app/src/components/Header/Header.jsx
@@ -1,7 +1,14 @@
 //import React from "react"
 import './Header.css'
+import { useState } from 'react'
 
 export function Header(props) {
+    const [showMenu, setShowMenu] = useState(false)
+
+    const handleClick = (event) => {
+        setShowMenu(!showMenu)
+    }
+
     const { content, color } = props
     const options = ["News", "Overview", "Community", "Support"]
 
@@ -13,27 +20,31 @@ export function Header(props) {
             alignItems: 'center',
             flexWrap: 'wrap',*/
         }}>
-            <img src={'https://pso2.com/players/update/ver2/img/logo_ngs_ver2.png'} style={{
+            <img className='icon' onClick={handleClick} src={'https://pso2.com/players/update/ver2/img/logo_ngs_ver2.png'} style={{
                 height: '9vh',
                 width: 'auto'
             }}></img>
 
-            <div className="optContainer" style={{
+            {
+                showMenu ?
+                <div className="optContainer" style={{
 
-            }}>
-                {
-                    options.map((option) =>
-                        <div className='dropdown'>
-                            <button className="opts" key={{ option }}>{option}</button>
-                            <div class="dropdown-content">
-                                <a href="#">Link 1</a>
-                                <a href="#">Link 2</a>
-                                <a href="#">Link 3</a>
+                }}>
+                    {
+                        options.map((option) =>
+                            <div className='dropdown'>
+                                <button className="opts" key={{ option }}>{option}</button>
+                                <div class="dropdown-content">
+                                    <a href="#">Link 1</a>
+                                    <a href="#">Link 2</a>
+                                    <a href="#">Link 3</a>
+                                </div>
                             </div>
-                        </div>
-                    )
-                }
-            </div>
+                        )
+                    }
+                </div> :
+                null
+            }
 
 
         </header>
