7f97309e03b2596691c201c1566a4211bbe3af69
Sebastian Romero <nakosume@gmail.com>
2024-02-19 18:46:21 -0500

    reinstalle ESLint

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React from "react"
-import { Header } from "./components/Header/Header"
-import { Main } from "./components/Main/Main"
-import { Footer } from "./components/Footer/Footer"
+import React from 'react'
+import { Header } from './components/Header/Header'
+import { Main } from './components/Main/Main'
+import { Footer } from './components/Footer/Footer'
 
+export default function App () {
+  return (
 
-export default function App() {
-
-    return (
-
-        <div>
-            <Header></Header>
-            <Main></Main>
-            <Footer></Footer>
-        </div>
-    )
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
 }
