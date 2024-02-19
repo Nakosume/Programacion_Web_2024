512ec13375856ed71e470803a7f352df9b858005
Sebastian Romero <nakosume@gmail.com>
2024-02-19 12:51:05 -0500

    Your alredy know, installing ESLint

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,52 +1,51 @@
 //import React from "react"
-import './Header.css'
-import { useState } from 'react'
+import "./Header.css";
+import { useState } from "react";
 
 export function Header(props) {
-    const [showMenu, setShowMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(false);
 
-    const handleClick = (event) => {
-        setShowMenu(!showMenu)
-    }
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
 
-    const { content, color } = props
-    const options = ["News", "Overview", "Community", "Support"]
+  const { color } = props;
+  const options = ["News", "Overview", "Community", "Support"];
 
-    return (
-        <header style={{
-            color: color,
-            /*display: 'flex',
+  return (
+    <header
+      style={{
+        color: color,
+        /*display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
             flexWrap: 'wrap',*/
-        }}>
-            <img className='icon' onClick={handleClick} src={'https://pso2.com/players/update/ver2/img/logo_ngs_ver2.png'} style={{
-                height: '9vh',
-                width: 'auto'
-            }}></img>
+      }}
+    >
+      <img
+        className="icon"
+        onClick={handleClick}
+        src={"https://pso2.com/players/update/ver2/img/logo_ngs_ver2.png"}
+        style={{
+          height: "9vh",
+          width: "auto",
+        }}
+      ></img>
 
-            {
-                showMenu ?
-                <div className="opt-container" style={{
-
-                }}>
-                    {
-                        options.map((option) =>
-                            <div className='dropdown'>
-                                <button className="opts" key={{ option }}>{option}</button>
-                                <div class="dropdown-content">
-                                    <a href="#">Link 1</a>
-                                    <a href="#">Link 2</a>
-                                    <a href="#">Link 3</a>
-                                </div>
-                            </div>
-                        )
-                    }
-                </div> :
-                null
-            }
-
-
-        </header>
-    )
+      {showMenu ? (
+        <div className="opt-container" style={{}}>
+          {options.map((option) => (
+            <div className="dropdown" key={option}>
+              <button className="opts">{option}</button>
+              <div className="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : null}
+    </header>
+  );
 }
