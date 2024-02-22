82d66c7c95260ddf1ed7b73d4ecba83c88d4bef9
Sebastian Romero <nakosume@gmail.com>
2024-02-21 19:49:34 -0500

    Header working as intended

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 // import React from "react"
 import './Header.css'
 import { useState } from 'react'
+import { LINKS } from '../../const/links.js'
 
 export function Header (props) {
   const [showMenu, setShowMenu] = useState(false)
@@ -10,7 +11,6 @@ export function Header (props) {
   }
 
   const { color } = props
-  const options = ['News', 'Overview', 'Community', 'Support']
 
   return (
     <header
@@ -32,18 +32,40 @@ export function Header (props) {
         src='https://pso2.com/players/update/ver2/img/logo_ngs_ver2.png'
       />
 
-      <div className={!showMenu ? 'opt-container hidden' : 'opt-container'}>
-        {options.map((option) => (
-          <div className='dropdown' key={option}>
-            <button className='opts'>{option}</button>
-            <div className='dropdown-content'>
-              <a href='#'>Link 1</a>
-              <a href='#'>Link 2</a>
-              <a href='#'>Link 3</a>
-            </div>
-          </div>
-        ))}
-      </div>
+      <Menu
+        options={LINKS}
+        showMenu={showMenu}
+      />
+
     </header>
+  )
+}
+
+function Menu ({ options, showMenu }) {
+  return (
+    <div className={!showMenu ? 'opt-container hidden' : 'opt-container'}>
+      {options.map((option) => (
+
+        <MenuItem item={option} key={option.id}>{option}</MenuItem>
+
+      ))}
+    </div>
+  )
+}
+
+function MenuItem ({ item }) {
+  const { text } = item
+  return (
+    <div className='dropdown'>
+      <button className='opts'>
+        {text}
+      </button>
+      <div className='dropdown-content'>
+        <a href='#'>Link 1</a>
+        <a href='#'>Link 2</a>
+        <a href='#'>Link 3</a>
+      </div>
+
+    </div>
   )
 }
