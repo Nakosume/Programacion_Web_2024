1de33f10604609d694b8b25c45edf037b1bb08f1
Sebastian Romero <nakosume@gmail.com>
2024-05-09 18:48:32 -0500

    debouncer go vrrrrrrrr

--- Gif_Search_App/src/components/DisplayGifs.jsx
+++ Gif_Search_App/src/components/DisplayGifs.jsx
@@ -1,17 +1,19 @@
 import { useGif } from '../hooks/useGif'
 import ImageItem from './ImageItem'
 
+// data.lenght > 0
+
 const DisplayGifs = () => {
-  const { loading, data } = useGif()
+  const { data } = useGif()
 
   return (
     <div className='container-gifs'>
       {
-                loading
+                data.length > 0
                   ? data.map(img => (
                     <ImageItem key={img.id} title={img.title} url={img.images.downsized_medium.url} />
                   ))
-                  : ''
+                  : <div>Oe mano, buscate alguito</div>
             }
     </div>
 
--- Gif_Search_App/src/context/GifContextProvider.jsx
+++ Gif_Search_App/src/context/GifContextProvider.jsx
@@ -1,15 +1,16 @@
 // Reducer & functions
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { GifContext } from './GifContext'
+import debounce from 'just-debounce-it'
 
-const key = 'Pa7OKX0j4Wf0lfKDrH40M76Cm9wrYrXQ'
+const key = '0OgQXOAJvufNbdsE54ptzagc6G8ZP9lu'
 
 export const GifContextProvider = ({ children }) => {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
   const [search, setSearch] = useState('')
 
-  const fetchApi = () => {
+  const fetchApi = (search) => {
     fetch(`https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${search}`)
       .then(res => res.json())
       .then(data => {
@@ -21,10 +22,10 @@ export const GifContextProvider = ({ children }) => {
       .catch(error => console.log(error))
   }
 
+  const debounceIt = useCallback(debounce((q) => fetchApi(q), 2000), [])
+
   useEffect(() => {
-    setTimeout(() => {
-      fetchApi()
-    }, 2000)
+    debounceIt(search)
   }, [search])
 
   return (
